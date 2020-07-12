84c5c23ccf6b728946e770a73751bf208da54ddb
LLY546498 <820954981@qq.com>
2020-07-12 17:23:55 +0800

    完成请求登录

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,11 +1,10 @@
  <template>
-  <div class="layout-container" fixed>
-    <van-tabbar v-model="active">
-      <!-- <van-tabbar-item replace to="/" icon="iconfont icon-shouye">首页</van-tabbar-item> -->
-      <van-tabbar-item replace to="/" class-prefix="icon" icon="iconfont icon-shouye">首页</van-tabbar-item>
-      <van-tabbar-item replace to="/qa" icon="iconfont icon-wenda">问答</van-tabbar-item>
-      <van-tabbar-item replace to="/video" icon="iconfont icon-shipin">视频</van-tabbar-item>
-      <van-tabbar-item replace to="/my" icon="iconfont icon-wode">我的</van-tabbar-item>
+  <div class="layout-container">
+    <van-tabbar v-model="active" fixed route>
+      <van-tabbar-item replace to="/" icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item replace to="/qa" icon="comment-o">问答</van-tabbar-item>
+      <van-tabbar-item replace to="/video" icon="video-o">视频</van-tabbar-item>
+      <van-tabbar-item replace to="/my" icon="manager-o">我的</van-tabbar-item>
     </van-tabbar>
     <router-view></router-view>
   </div>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,24 +3,31 @@
     <!-- 导航栏 -->
     <van-nav-bar
       class="page-nav-bar"
-      title="登录"
+      title="注册 / 登录"
     />
-    <van-form @submit="onSubmit">
+    <van-form>
     <van-field
-      v-model="username"
-      name="用户名"
+      v-model="user.mobile"
+      name="手机名"
       placeholder="请输入手机号"
-      :rules="[{ required: true, message: '请输入手机号' }]"
-    />
+    >
+    <i slot="left-icon" class="iconfont icon-shouji"></i>
+    </van-field>
     <van-field
-      v-model="password"
-      type="password"
-      name="密码"
+      v-model="user.code"
+      type="number"
+      name="验证码"
       placeholder="请输入验证码"
-      :rules="[{ required: true, message: '请输入验证码' }]"
-    />
-    <div style="margin: 16px;">
-      <van-button block type="info" native-type="submit">
+    >
+    <i slot="left-icon" class="iconfont icon-yanzhengma"></i>
+    <template #button>
+      <div class="btn-b">
+        <van-button class="yan-btn" round size="small" color="#eee" type="default">发送验证码</van-button>
+      </div>
+    </template>
+    </van-field>
+    <div style="margin: 20px;">
+      <van-button @click="onLogin" block type="info"  native-type="submit">
         登录
       </van-button>
     </div>
@@ -29,14 +36,17 @@
 </template>
 
 <script>
+import { login } from '@/api/user'
 export default {
   name: 'LoginIndex',
   components: {},
   props: {},
   data () {
     return {
-      username: '',
-      password: ''
+      user: {
+        mobile: '',
+        code: ''
+      }
     }
   },
   computed: {},
@@ -44,13 +54,40 @@ export default {
   created () {},
   mounted () {},
   methods: {
-    onSubmit (values) {
-      console.log('submit', values)
+    async onLogin () {
+      // 1,找到数据接口，
+      // 2，封装请求方法，
+      // 3，请求调用，
+      // 4，处理响应结果
+      try {
+        const res = await login(this.user)
+        console.log('登录成功', res)
+      } catch (err) {
+        if (err.response.status === 400) {
+          console.log('登陆失败', err)
+        }
+      }
     }
   }
 }
 </script>
 
 <style scoped lang="less">
-
+.iconfont {
+  font-size: 40px;
+}
+.btn-b {
+  width: 152px;
+  height: 46px;
+  padding-left: 14px;
+  border-left: 2px solid #eee;
+  .yan-btn {
+  width: 152px;
+  height: 46px;
+  background-color: #eee;
+  line-height: 46px;
+  font-size: 22px;
+  color: #666!important;
+  }
+}
 </style>
